b772cdab09088b972f8e588cd11f9e3a87be2b37
CCNCAY <peter.horvath@live.com>
2024-05-08 23:32:22 +0200

    first steps

---
+++ bgbuddy/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace bgbuddy
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+
+
+    {
+        public string TestString = "Test";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddFriendButton(object sender, RoutedEventArgs rea)
+        {
+            MainTextBox.Text = TestString;
+        }
+
+        private void AddGameButton(object sender, RoutedEventArgs rea)
+        {
+
+        }
+
+
+    }
+}
